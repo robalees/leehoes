3a41d8e2cf5944f027cb43713afa04ef515550f4
robalees <robalees@gmail.com>
2022-11-09 19:12:22 -0500

    Adding the gallery to dev3

---
+++ scripts/app.js
@@ -0,0 +1,64 @@
+const container = document.querySelector('.gallery-container');
+
+function generateMasonryGrid(columns, posts) {
+    container.innerHTML = '';
+    console.log(posts)
+    // store all column arrays which contain the relevant posts
+    let columnWrappers = {};
+    
+    //create column item array and add this to the column wrapper object
+    for(let i = 0; i < columns; i++){
+        columnWrappers[`column${i}`] = []
+    }
+
+    for(let i = 0; i < posts.length; i++ ){
+        const column = i % columns;
+        columnWrappers[`column${column}`].push(posts[i]);
+    }
+
+    for (let i = 0; i < columns; i++){
+        let columnPosts = columnWrappers[`column${i}`];
+        let column = document.createElement('div');
+        column.classList.add('column');
+
+        columnPosts.forEach(post => {
+            let postDiv = document.createElement('div');
+            postDiv.classList.add('post');
+            let image = document.createElement('img');
+            image.src = post.image;
+            let overlay = document.createElement('div');
+            overlay.classList.add('overlay');
+            let title = document.createElement('h3');
+            title.innerText = post.title;
+
+            overlay.appendChild(title);
+            postDiv.append(image, overlay);
+            column.appendChild(postDiv);
+        })
+    container.appendChild(column)
+    }
+}
+
+let previousScreenSize = window.innerWidth;
+
+window.addEventListener('resize', () => {
+    imageIndex = 0;
+    if(window.innerWidth < 600 && previousScreenSize >= 600){
+        generateMasonryGrid(1, posts);
+    }else if(window.innerWidth >= 600 && window.innerWidth < 1000 && (previousScreenSize < 600 || previousScreenSize >= 1000)){
+        generateMasonryGrid(2, posts);
+    }else if(window.innerWidth >= 1000 && previousScreenSize < 1000){
+        generateMasonryGrid(4, posts)
+    }
+    previousScreenSize = window.innerWidth
+})
+
+// Page load 
+
+if(previousScreenSize < 600){
+    generateMasonryGrid(1, posts);
+}else if(previousScreenSize >= 600 && previousScreenSize < 1000){
+    generateMasonryGrid(2, posts);
+}else{
+    generateMasonryGrid(4, posts);
+}
---
+++ scripts/items.js
@@ -0,0 +1,31 @@
+const posts = [];
+
+const images = [
+    '/images/wedding-images/wedding1.jpg',
+    '/images/wedding-images/wedding2.jpg',
+    '/images/wedding-images/wedding3.jpg',
+    '/images/wedding-images/wedding4.jpg',
+    '/images/wedding-images/wedding5.jpg',
+    '/images/wedding-images/wedding6.jpg',
+    '/images/wedding-images/wedding7.jpg',
+    '/images/wedding-images/wedding8.jpg',
+    '/images/wedding-images/wedding9.jpg',
+    '/images/wedding-images/wedding10.jpg',
+    '/images/wedding-images/wedding11.jpg',
+    '/images/wedding-images/wedding12.jpg',
+    '/images/wedding-images/wedding13.jpg',
+    '/images/wedding-images/wedding14.jpg',
+];
+
+let imageIndex = 0;
+
+for(let i = 1; i <= 80; i++){
+    let item = {
+        id: i,
+        title: `Post ${i}`,
+        image: images[imageIndex]
+    }
+    posts.push(item);
+    imageIndex++;
+    if(imageIndex > images.length - 1) imageIndex = 0;
+}
